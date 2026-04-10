df63a994c2b9b16b95f3c8edde7a0282e8b2e929
Nick Chadwick <chadnickbok@gmail.com>
2026-04-10 00:43:13 -0700

    Remove bad publisherName for windows

--- apps/desktop/scripts/run-electron-builder.mjs
+++ apps/desktop/scripts/run-electron-builder.mjs
@@ -55,7 +55,6 @@ if (process.env.DESKTOP_WINDOWS_SIGN === 'true') {
   }
 
   args.push(
-    `-c.win.publisherName=${process.env.WINDOWS_SIGN_PUBLISHER_NAME}`,
     `-c.win.azureSignOptions.endpoint=${process.env.WINDOWS_AZURE_TRUSTED_SIGNING_ENDPOINT}`,
     `-c.win.azureSignOptions.codeSigningAccountName=${process.env.WINDOWS_AZURE_TRUSTED_SIGNING_ACCOUNT_NAME}`,
     `-c.win.azureSignOptions.certificateProfileName=${process.env.WINDOWS_AZURE_TRUSTED_SIGNING_PROFILE_NAME}`,
